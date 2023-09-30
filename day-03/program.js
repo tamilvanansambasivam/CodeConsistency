637949479f8bae69e80d0046218c9b86ef3d0fee
tamilvanan <tamilvanan@Mac-mini.local>
2023-09-30 21:14:57 +0530

    use HOF

--- day-03/program.js
+++ day-03/program.js
@@ -1,22 +1,18 @@
+const radius = [1, 2, 3, 4];
+const diameter = function (r) {
+  return 2 * r;
+};
 
-const radius=[1,2,3,4]
-
-function diameterOfCircle(arr){
-    ans=[]
-    for(let i=0;i<arr.length;i++){
-        ans.push(arr[i]*2);
-    }
-    return ans;
+const areaOfCircle = function (r) {
+  return Math.PI * r ** 2;
+};
+function calculate(arr, logic) {
+  ans = [];
+  for (let i = 0; i < arr.length; i++) {
+    ans.push(logic(radius[i]));
+  }
+  return ans;
 }
 
-console.log(diameterOfCircle(radius));
-
-function areaOfCircle(arr){
-    ans=[]
-    for(let i=0;i<arr.length;i++){
-        ans.push(Math.PI*arr[i]**2);
-    }
-    return ans;
-}
-
-console.log(areaOfCircle(radius));
+console.log(calculate(radius, diameter));
+console.log(calculate(radius, areaOfCircle));
